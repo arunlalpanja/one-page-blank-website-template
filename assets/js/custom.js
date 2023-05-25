e2edc66e5899920737ae43ff7167c431822aedc4
Arunlal Panja <arunlalpanja@live.com>
2023-05-25 12:17:11 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,71 @@
+/* Custom Script */
+/* Owl Carousel V2 */
+$('.owl-carousel-v2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    center: true,
+    dots: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 7
+        }
+    }
+});
+/* Owl Carousel */
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    center: true,
+    dots: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+/* Animate On Scroll */
+AOS.init();
+
+/* light Gallery */
+lightGallery(document.getElementById('lightgallery'), {
+    subHtmlSelectorRelative: true,
+});
+
+/* Scroll Top */
+$(document).ready(function() {
+    "use strict";
+    var offSetTop = 300;
+    var $scrollToTopButton = $('.scrollToTop');
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offSetTop) {
+            $scrollToTopButton.fadeIn();
+        } else {
+            $scrollToTopButton.fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $scrollToTopButton.click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 200);
+        return false;
+    });
+
+});
